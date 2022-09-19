00b8452412bdb6b500c6561848a14a4dd5e2b30b
hdev299 <hartaiakos29@gmail.com>
2022-09-19 21:17:49 +0200

    Kommenteltem amit kommentelni kellett

--- main.cpp
+++ main.cpp
@@ -4,20 +4,28 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+	 //Elírás. Ide az "N_ELEMENTS" kellene.
     int *b = new int[NELEMENTS];
+	//Nincs sorlezárás
     std::cout << '1-100 ertekek duplazasa'
+	//Hiányos for ciklus.
     for (int i = 0;)
     {
+		//0-tól 99-ig duplázunk és nem 1-től 100-ig
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
+		//Magát az értéket ki sem írjuk, és nincs sorlezárás sem.
         std::cout << "Ertek:"
-    }    
+    }
+	//Nincs kezdőértéke a változónak.    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
+	//Pontosvessző helyett sima vessző található a lépésköz előtt.
     for (int i = 0; i < N_ELEMENTS, i++)
     {
+		//Hiányzó sorlezárás.
         atlag += b[i]
     }
     atlag /= N_ELEMENTS;
