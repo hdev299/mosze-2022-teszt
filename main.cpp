0a032c1876bdb8fc13bac107c84a71cdec5dc13d
hdev299 <hartaiakos29@gmail.com>
2022-09-19 21:23:08 +0200

    Javítottam amit javítani kellett

--- main.cpp
+++ main.cpp
@@ -4,31 +4,31 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-	 //Elírás. Ide az "N_ELEMENTS" kellene.
-    int *b = new int[NELEMENTS];
-	//Nincs sorlezárás
-    std::cout << '1-100 ertekek duplazasa'
-	//Hiányos for ciklus.
-    for (int i = 0;)
+    //Elírás. Ide az "N_ELEMENTS" kellene.
+    int *b = new int[N_ELEMENTS];
+    //Nincs sorlezárás
+    std::cout << '1-100 ertekek duplazasa' << std::endl;
+    //Hiányos for ciklus.
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-		//0-tól 99-ig duplázunk és nem 1-től 100-ig
-        b[i] = i * 2;
+        //0-tól 99-ig duplázunk és nem 1-től 100-ig
+        b[i] = (i + 1) * 2;
     }
-    for (int i = 0; i; i++)
+    //Ebbe a ciklusba be sem lépünk.
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-		//Magát az értéket ki sem írjuk, és nincs sorlezárás sem.
-        std::cout << "Ertek:"
-    }
-	//Nincs kezdőértéke a változónak.    
+        //Magát az értéket ki sem írjuk, és nincs sorlezárás sem.
+        std::cout << "Ertek:" << b[i] << std::endl;
+    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-	//Pontosvessző helyett sima vessző található a lépésköz előtt.
-    for (int i = 0; i < N_ELEMENTS, i++)
+    //Nincs kezdőértéke a változónak.
+    int atlag = 0;
+    //Pontosvessző helyett sima vessző található a lépésköz előtt.
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-		//Hiányzó sorlezárás.
-        atlag += b[i]
+        //Hiányzó sorlezárás.
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
 }
